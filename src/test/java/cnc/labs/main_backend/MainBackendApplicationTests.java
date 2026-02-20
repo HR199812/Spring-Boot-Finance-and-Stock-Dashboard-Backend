53b18d547b533c539a29d67bd478ea87d5ffd40e
hritwik agarwal <hritwik72@gmail.com>
2026-02-20 11:08:57 +0530

    initial commit

---
+++ src/test/java/cnc/labs/main_backend/MainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cnc.labs.main_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
